b78b3e78ec72582dac8f96d1cef463f9bb06ce25
Moritz <mail@moritzhorn.de>
2024-10-05 04:00:54 +0200

    regarding sell order now with map instead of Dict

--- src/app/(loggedin)/(contentpages)/league/[leagueId]/tradehistory/[managerId]/page.tsx
+++ src/app/(loggedin)/(contentpages)/league/[leagueId]/tradehistory/[managerId]/page.tsx
@@ -73,23 +73,24 @@ export default function TradeHistory({
 
   //compute all pairs of trades and the current squad
 
-  let finalPairs: { [key: string]: [string, string] } = {}; //map to store the final pairs of trades and their corresponding squad, usage: player_id -> [buy_value, sell_value]
-  let currentSquad: { [key: string]: string } = {}; //map to store the current squad, usage: player_id -> buy_value
+  let finalPairs: Map<string, [string, string]> = new Map(); //map to store the final pairs of trades and their corresponding squad, usage: player_id -> [buy_value, sell_value]
+  let currentSquad: Map<string, string> = new Map(); //map to store the current squad, usage: player_id -> buy_value
   list.forEach((trade: any) => {
     console.log("TRADE ID: ", trade.pi);
     if (trade.tty === 2) {
       //if trade is a sell, it has to be owned some time before
-      finalPairs[trade.pi] = [undefined, trade.trp];
+      finalPairs.set(trade.pi, [undefined, trade.trp]);
     }
     if (trade.tty === 1 || trade.tty === 0) {
       //if trade is a buy, we look if it has been sold already (so if the key already exists in the dict), if not, it has to be in the squad
-      if (finalPairs[trade.pi]) {
-        finalPairs[trade.pi][0] = trade.trp;
+      if (finalPairs.get(trade.pi) != undefined) {
+        finalPairs.set(trade.pi, [trade.trp, finalPairs.get(trade.pi)[1]]);
       } else {
-        currentSquad[trade.pi] = trade.trp;
+        currentSquad.set(trade.pi, trade.trp);
       }
     }
   });
+  console.log("FINAL PAIRS: ", finalPairs);
 
   return (
     <div className="flex flex-col justify-center items-center">
@@ -97,15 +98,15 @@ export default function TradeHistory({
         {/*history[0] because the first entry of the chained requests list will always exist and contains the name in unm param */}
         Trade History for {history ? history[0].unm : ""}
       </h2>
-      {Object.keys(finalPairs).map((key) => {
+      {Array.from(finalPairs.keys()).map((key) => {
         console.log("ABOUT TO MAKE TRADE HISTORY ELEMENT", key);
         return (
           <div key={key}>
             <TransferHistoryElement
               leagueId={params.leagueId}
               playerId={key}
-              buyValue={finalPairs[key][0]}
-              sellValue={finalPairs[key][1]}
+              buyValue={finalPairs.get(key)[0]}
+              sellValue={finalPairs.get(key)[1]}
             />
           </div>
         );
